5bc3e57ebb7ae83585821136df62f4c7253020f4
apoorvaron <apoorv.aron02@gmail.com>
2022-08-15 14:56:36 +0530

    web developer & analyist animated

---
+++ assets/js/top.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typing-text", {
+  strings: ["Full-Stack Web Developer", "Data Stuctures Analyist"],
+  loop: true,
+  typeSpeed: 50,
+  backSpeed: 25,
+  backDelay: 500,
+});

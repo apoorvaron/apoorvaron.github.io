27bb434a3ef524f0159ed2021e5274ce6f83e442
apoorvaron <apoorv.aron02@gmail.com>
2022-08-19 23:56:26 +0530

    Running number

--- assets/js/home_new.js
+++ assets/js/home_new.js
@@ -441,3 +441,19 @@ $(document).on("scroll", function () {
     }
   }
 });
+$(".count--run").each(function () {
+  $(this)
+    .prop("Counter", 0)
+    .animate(
+      {
+        Counter: $(this).text(),
+      },
+      {
+        duration: 4000,
+        easing: "swing",
+        step: function (now) {
+          $(this).text(Math.ceil(now));
+        },
+      }
+    );
+});

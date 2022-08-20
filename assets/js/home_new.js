94f63e10fdd7d7b5e696a934e41a5fa0e314035c
apoorvaron <apoorv.aron02@gmail.com>
2022-08-20 20:19:42 +0530

    timeline added

--- assets/js/home_new.js
+++ assets/js/home_new.js
@@ -457,3 +457,18 @@ $(".count--run").each(function () {
       }
     );
 });
+document.addEventListener("scroll", timeline);
+
+function timeline() {
+  var threshold_position = window.scrollY + (window.innerHeight * 2) / 3;
+  //compare scrolltop with scrolltop on each timeline event
+  var timeline_events = document.querySelectorAll(".timeline li");
+  for (i in timeline_events) {
+    if (timeline_events[i].offsetTop < threshold_position) {
+      timeline_events[i].classList.add("visible");
+    } else {
+      timeline_events[i].classList.remove("visible");
+    }
+  }
+}
+timeline();

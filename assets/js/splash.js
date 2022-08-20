860fbf3005f5ac52017b46febc5fb2e28e0ac980
apoorvaron <apoorv.aron02@gmail.com>
2022-08-20 13:45:03 +0530

    splash screen half done

---
+++ assets/js/splash.js
@@ -0,0 +1,27 @@
+$(".loadingbar").delay(1500).animate({ left: "0" }, 3000);
+$(".loadingBox").delay(500).animate({ opacity: "1" }, 1000);
+// $(".splashScreen").delay(4500).animate({ top: "-100%" }, 1500);
+$(".loadingCircle").delay(4500).animate({ opacity: "0" }, 500);
+$("body")
+  .delay(5000)
+  .queue(function () {
+    // window.location.replace("https://apoorvaron.me");
+    window.location = "./index.html";
+    // $(".chupahuahai").css("display", "inline-block");
+  });
+$(".count--splash").each(function () {
+  $(this)
+    .prop("Counter", 0)
+    .animate(
+      {
+        Counter: $(this).text(),
+      },
+      {
+        duration: 5000,
+        easing: "swing",
+        step: function (now) {
+          $(this).text(Math.ceil(now));
+        },
+      }
+    );
+});
